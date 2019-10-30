e4388e14e9ff113d39f7fb491b98a38aa017a0e0
shivampanwar1122 <57106166+shivampanwar1122@users.noreply.github.com>
2019-10-30 13:41:35 +0530

    Add files via upload

---
+++ reversestring.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,m;
+char x[20];
+printf("enter the number of string:");
+scanf("%d",&n);
+printf("enter the string:")
+for(i=0;i<n;i++)
+    scanf("%c",x[i]);
+m=strlen(x);
+for(i=m-1;i>=0;i--)
+printf("%c",x[i]);
+return 0;
+
+}
